ae3ec7484d84dc2ac59ca571b4b9a96929dd3fc2
TheKods <rafihermawan06@gmail.com>
2026-08-23 16:38:47 +0700

    Update AIDetectionBox.h

--- src/AI/AIDetectionBox.h
+++ src/AI/AIDetectionBox.h
@@ -2,6 +2,8 @@
 
 #include <QtCore/QObject>
 #include <QtCore/QString>
+#include <QtCore/QPointF>
+#include <QtCore/QList>
 #include <QtPositioning/QGeoCoordinate>
 #include <QtQmlIntegration/QtQmlIntegration>
 
@@ -17,11 +19,14 @@ class AIDetectionBox : public QObject
     Q_PROPERTY(double y READ y NOTIFY geometryChanged)
     Q_PROPERTY(double width READ width NOTIFY geometryChanged)
     Q_PROPERTY(double height READ height NOTIFY geometryChanged)
+    Q_PROPERTY(double centerX READ centerX NOTIFY geometryChanged)
+    Q_PROPERTY(double centerY READ centerY NOTIFY geometryChanged)
     Q_PROPERTY(bool isLocked READ isLocked WRITE setIsLocked NOTIFY isLockedChanged)
     Q_PROPERTY(QString trackingStatus READ trackingStatus NOTIFY trackingStatusChanged)
     Q_PROPERTY(QGeoCoordinate coordinate READ coordinate NOTIFY coordinateChanged)
     Q_PROPERTY(double rangeMeters READ rangeMeters NOTIFY rangeMetersChanged)
     Q_PROPERTY(double estimatedSpeedKmh READ estimatedSpeedKmh NOTIFY estimatedSpeedKmhChanged)
+    Q_PROPERTY(double headingDeg READ headingDeg NOTIFY headingDegChanged)
     Q_PROPERTY(QString boxColorHex READ boxColorHex NOTIFY boxColorChanged)
 
 public:
@@ -39,11 +44,14 @@ public:
     [[nodiscard]] double y() const { return _y; }
     [[nodiscard]] double width() const { return _width; }
     [[nodiscard]] double height() const { return _height; }
+    [[nodiscard]] double centerX() const { return _x + (_width / 2.0); }
+    [[nodiscard]] double centerY() const { return _y + (_height / 2.0); }
     [[nodiscard]] bool isLocked() const { return _isLocked; }
     [[nodiscard]] QString trackingStatus() const { return _trackingStatus; }
     [[nodiscard]] QGeoCoordinate coordinate() const { return _coordinate; }
     [[nodiscard]] double rangeMeters() const { return _rangeMeters; }
     [[nodiscard]] double estimatedSpeedKmh() const { return _estimatedSpeedKmh; }
+    [[nodiscard]] double headingDeg() const { return _headingDeg; }
     [[nodiscard]] QString boxColorHex() const;
 
     void updateData(const QString &className, double confidence,
@@ -53,6 +61,7 @@ public:
     void setCoordinate(const QGeoCoordinate &coord);
     void setRangeMeters(double range);
     void setEstimatedSpeedKmh(double speed);
+    void setHeadingDeg(double heading);
 
 signals:
     void classNameChanged();
@@ -63,6 +72,7 @@ signals:
     void coordinateChanged();
     void rangeMetersChanged();
     void estimatedSpeedKmhChanged();
+    void headingDegChanged();
     void boxColorChanged();
 
 private:
@@ -78,4 +88,7 @@ private:
     QGeoCoordinate _coordinate;
     double _rangeMeters = 0.0;
     double _estimatedSpeedKmh = 0.0;
+    double _headingDeg = 0.0;
+    double _prevCenterX = 0.0;
+    double _prevCenterY = 0.0;
 };
